122091cbf73bb3931157092fe321070d57e4b1fd
brenward <bren.ward@gmail.com>
2024-04-13 17:01:27 +0100

    #Using a subject in place of an eventemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated:boolean = false;
+  private activatedSub: Subscription;
+  constructor(private userService:UserService) {}
+  
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activaterEmitter.subscribe((data:boolean) => {
+      this.userActivated = data;
+    });
+  }
 }
